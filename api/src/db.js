49c601c6f73d204918eae6a3b6c4aaa9bb75743d
johalama2023 <johalama2023@gmail.com>
2023-06-06 08:19:17 -0500

    cambios

--- api/src/db.js
+++ api/src/db.js
@@ -2,16 +2,15 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
-// cambcianod estru
 const sequelize = new Sequelize(
-  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/${DB_DATABASE}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-);  
+);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
